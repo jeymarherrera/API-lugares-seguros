f659a7a96d093f9184f94c7e21f0426a5bba4c6e
jeymarherrera <jeymarherrera@gmail.com>
2023-02-23 14:50:46 -0500

    Create server

---
+++ config/config.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+//exportacion de codigo al resto del proyecto
+module.exports = {
+    PORT: process.env.PORT,
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+const {PORT} = require("./config/config");
+//inicializar nuestro servidor
+const express = require("express");
+const server = express();
+
+server.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+})
